71767be22a3fdc9a6041bb34563cbd74a310affb
fenroe <scrambles9494@gmail.com>
2025-04-09 23:25:30 +0300

    bad bad wednesday

--- molly-web/src/pages/Signup.tsx
+++ molly-web/src/pages/Signup.tsx
@@ -8,12 +8,21 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { FormEvent } from "react";
 import { Link } from "react-router";
 import { Formik, Form } from "formik";
 
+interface Values {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 export const Signup = () => {
-  const handleSignup = () => {};
+  const handleSignup = (values: Values) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+    }, 500);
+  };
 
   return (
     <main className="min-h-[calc(100vh-80px)] bg-amber-50 flex flex-col items-center py-6 px-3">
@@ -27,57 +36,58 @@ export const Signup = () => {
               <Formik
                 initialValues={{ email: "", password: "", confirmPassword: "" }}
                 onSubmit={handleSignup}
-              ></Formik>
-              <form onSubmit={handleSignup}>
-                <div className="flex flex-col gap-6">
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="m@example.com"
-                      required
-                    />
+              >
+                <Form>
+                  <div className="flex flex-col gap-6">
+                    <div className="grid gap-3">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor="password">Password</Label>
+                      <Input id="password" type="password" required />
+                    </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor="confirmPassword">Confirm Password</Label>
+                      <Input id="confirmPassword" type="password" required />
+                    </div>
+                    <div className="flex flex-col gap-3">
+                      <Button type="submit" className="w-full">
+                        Sign up
+                      </Button>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <div className="">
+                              <Button
+                                variant="outline"
+                                className="w-full"
+                                disabled
+                              >
+                                Login with Google
+                              </Button>
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Coming soon!</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
                   </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="password">Password</Label>
-                    <Input id="password" type="password" required />
+                  <div className="mt-4 text-center text-sm">
+                    Already have an account?{" "}
+                    <Link to="/login" className="underline underline-offset-4">
+                      Log in
+                    </Link>
                   </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="confirmPassword">Confirm Password</Label>
-                    <Input id="confirmPassword" type="password" required />
-                  </div>
-                  <div className="flex flex-col gap-3">
-                    <Button type="submit" className="w-full">
-                      Sign up
-                    </Button>
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <div className="">
-                            <Button
-                              variant="outline"
-                              className="w-full"
-                              disabled
-                            >
-                              Login with Google
-                            </Button>
-                          </div>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Coming soon!</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </div>
-                </div>
-                <div className="mt-4 text-center text-sm">
-                  Already have an account?{" "}
-                  <Link to="/login" className="underline underline-offset-4">
-                    Log in
-                  </Link>
-                </div>
-              </form>
+                </Form>
+              </Formik>
             </CardContent>
           </Card>
         </div>
